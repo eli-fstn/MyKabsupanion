a0d72bd92a6e6b2f8f7de7b3044e5f6ba7d962f0
skywie <festinelijah@gmail.com>
2026-06-13 17:44:52 +0800

    feat: admin pages, error pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import LogIn from "./pages/student/LogIn";
 import Dashboard from "./pages/student/Dashboard";
+import AdminLogIn from "./pages/admin/AdminLogIn";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import Error503 from "./pages/errors/Error503.jsx";
+import Error404 from "./pages/errors/Error404.jsx";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LogIn />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/adminlogin" element={<AdminLogIn />} />
+      <Route path="/admindashboard" element={<AdminDashboard />} />
+      <Route path="/error/503" element={<Error503 />} />
+      <Route path="/error/404" element={<Error404 />} />
     </Routes>
   );
 }
